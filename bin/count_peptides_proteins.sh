90858c8959843dc807767c7a2da0db4942dbbd1f
William Fondrie <fondriew@gmail.com>
2022-08-11 11:21:07 -0700

    Fix counting script

--- bin/count_peptides_proteins.sh
+++ bin/count_peptides_proteins.sh
@@ -3,21 +3,19 @@
 QUERY=$(cat <<EOF
 SELECT
     count(distinct p.ProteinGroup),
-    count(distinct p2p.PeptideSeq )
+    count(distinct p2p.PeptideSeq)
 FROM proteinscores p, peptidetoprotein p2p
 WHERE p.ProteinAccession=p2p.ProteinAccession
     and p2p.isDecoy == 0
 EOF
 )
 
-echo QUERY
-
-function count_unique_peptides_proteins() {
+function count_unique_peptides_proteins {
     [[ $# -eq 0 ]] && return
     FILE=$1
-    sqlite3 $FILE ${QUERY} \
+    sqlite3 $FILE "${QUERY}" \
         | awk -v FILE="${FILE}" \
             '{split($0,a,"|"); printf "%s,%d,%d\n",FILE,a[1],a[2]}'
 }
 
-count_unique_peptides_proteins
+count_unique_peptides_proteins $1
